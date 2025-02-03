1922a113bfca007d93b98a92a20d2d2f783289ef
lmatiasbelderrain <Matibelde@hotmail.com>
2025-02-03 17:59:54 +0000

    GeneradorDeDominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my"];
+  let adj = ["great", "big", "small", "white"];
+  let noun = ["jogger", "racoon", "dog"];
+
+  // dominios
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        console.log(pronoun[i] + adj[b] + noun[c] + ".com.uy");
+      }
+    }
+  }
 };
